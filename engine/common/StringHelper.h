b01f11362d2a1cfaf562067a7f24cc4ecc3e3612
ChangHangeol <hgchang1@naver.com>
2024-10-09 23:26:32 +0900

    -

---
+++ engine/common/StringHelper.h
@@ -0,0 +1,6 @@
+#ifndef STRING_HELPER_H
+#define STRING_HELPER_H
+
+#include <string>
+
+#endif  // STRING_HELPER_H
--- engine/interface/BakeBehaviour.h
+++ engine/interface/BakeBehaviour.h
@@ -1,4 +1,10 @@
+#ifndef BAKE_BEHAVIOUR_H
+#define BAKE_BEHAVIOUR_H
 
+#include <string>
+#include <map>
+
+#include "BiscuitEngine.h"
 
 // 유니티의 MonoBehaviour와 비슷한 역할을 하는 클래스
 class BakeBehaviour {
@@ -8,11 +14,18 @@ public:
         // 업데이트를 사용할 떄와, 사용하지 않을 때의 등록을 나눌 것.
     }
 
-    virtual void Awake();
-    virtual void Start();
-    virtual void Update();
+    virtual void Awake() {};
+    virtual void Start() {};
+    virtual void Update() {};
+
+protected:
+    // static하게 띄워서 
+    static BakeBehaviour *FindBehaviour(std::string *name);
+    static std::map<size_t, BakeBehaviour*> behaviours; // hashcode, BakeBehaviour
 
 private:
 
 
 };
+
+#endif // BAKE_BEHAVIOUR_H 
