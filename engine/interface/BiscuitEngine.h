21618dec1d0924db48de906deaa444f3ccbd9979
ChangHangeol <hgchang1@naver.com>
2024-10-11 22:44:11 +0900

    [STRUCT] biscuit engine singleton

--- engine/interface/BiscuitEngine.h
+++ engine/interface/BiscuitEngine.h
@@ -1,8 +1,19 @@
 
 // editor, gui에서 사용할 api class
+// static 하게 떠있어야함.
 class BiscuitEngine {
 public:
-    
-private:
+    static BiscuitEngine& GetInstance() {
+        static BiscuitEngine instance;
+        return instance;
+    }
+    BiscuitEngine(BiscuitEngine const&) = delete;
+    void operator=(BiscuitEngine const&) = delete;
 
+    // engine api
+
+
+private:
+    BiscuitEngine() {}
+    ~BiscuitEngine() {}
 };
