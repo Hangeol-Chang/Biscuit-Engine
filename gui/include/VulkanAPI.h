504b8865a26e7199f3362b3429613951251a6ada
hihangeol <hgchang1@naver.com>
2024-10-21 08:29:57 +0900

    [HOTFIX] fix VulkanAPI.h includePath

--- gui/include/VulkanAPI.h
+++ gui/include/VulkanAPI.h
@@ -13,7 +13,7 @@
 #include <vulkan/vulkan.h>
 #include <GLFW/glfw3.h>
 #include <GLFW/glfw3native.h>
-#include <glm./glm.hpp>
+#include <glm/glm.hpp>
 
 #include "VulkanHelper.h"
 #include "VulkanTypes.h"
