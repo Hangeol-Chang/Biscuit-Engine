3a9202c2cf1984f5c8eb4c176030ec6151a9d9bd
ChangHangeol <hgchang1@naver.com>
2024-10-23 23:28:21 +0900

    [STRUCT] gui component 클래스 제작 중

--- engine/interface/types.h
+++ engine/interface/types.h
@@ -5,64 +5,89 @@
 // engine에서 사용될 types들
 #include <string>
 #include <vector>
+#include <glm/glm.hpp>
+#include <map>
+#include <memory>
+
+#include "BakeBehaviour.h"
 
 namespace engine {
 
-    // about model
+    ///////////////// about model /////////////////
     typedef struct ModelData_Dynamic {
-        std::vector<glm::Vec3> vertices;
+        std::vector<glm::vec3> vertices;
         std::vector<uint32_t> indices;
-        std::vector<glm::Vec2> uvs;
+        std::vector<glm::vec2> uvs;
     } ModelData_Dynamic;
 
     typedef struct ModelData_Static {
         std::string modelFile;
     } ModelData_Static;
 
-    template <typename T>
+    template <typename T>   // T as ModelData_Dynamic, ModelData_Static
     class Model {
+    public:
+        void UpdateData(T data) { this->data = data; }
+    private:
         std::string mode;
-        glm::Vec3 position;
-        glm::Vec3 rotation;
-        glm::Vec3 scale;
+        glm::vec3 position;
+        glm::vec3 rotation;
+        glm::vec3 scale;
         T data;
+
     };
 
-
-    // about texture
+    ///////////////// about texture /////////////////
     typedef struct TextureData_Color {
-        glm::Vec4 color;
+        glm::vec3 color;
     } TextureData_Color;
 
     typedef struct TextureData_Image {
         std::string imageFile;
     } TextureData_Image;
 
-    template <typename T>
+    template <typename T>   // T as TextureData_Color, TextureData_Image
     class Texture {
+    public:
+        void UpdateData(T data) { this->data = data; }
+    private:
         std::string mode;
         T data;
-    }
+    };
 
-    // about shader
+    ///////////////// about shader /////////////////
     class Shader {
         std::string vertexShader;
         std::string fragmentShader;
         // 나중에 추가할 거 생기면 추가할 것.
-    }
+    };
     
+    ///////////////// Component /////////////////
+    class ComponentBase {
+    public:
+        virtual ~ComponentBase() = default;
 
-    // 이 class가 그대로 렌더링에 사용될 수 있도록 하는게 베스트
-    // Model mode, Texture mode
-    template <typename MM, typename TM>
-    class Component {
-        std::string behaviour;
-        std::string name;
+    protected:
+        std::string behaviourName;
         std::string parent;
-        // std::vector<std::string> children;
-        std::map<std::string, Component> children;
+        std::string name;
+        std::shared_ptr<BakeBehaviour> behaviour;
         
-        Model<MM> model
+        std::map<std::string, std::shared_ptr<ComponentBase>> parent;
+        std::map<std::string, std::shared_ptr<ComponentBase>> children;
+    };
+
+    template <typename MM, typename TM>
+    class Component : public ComponentBase {
+    public:
+        void UpdateModel(MM model) { this->model.UpdateData(model); }
+        void UpdateTexture(TM texture) { this->texture.UpdateData(texture); }
+
+        Model<MM> GetModel() { return model; }
+        Texture<TM> GetTexture() { return texture; }
+
+    private:
+        Model<MM>   model;
         Texture<TM> texture;
 
         Shader shader;
