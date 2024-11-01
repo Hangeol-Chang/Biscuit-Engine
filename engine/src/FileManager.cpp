a47129537d447565bb97c0c6aa6a33b7dc3e0f91
ChangHangeol <hgchang1@naver.com>
2024-11-01 19:58:44 +0900

    실행안되던거 해결함. 원인 몰?루

--- engine/src/FileManager.cpp
+++ engine/src/FileManager.cpp
@@ -11,20 +11,22 @@ namespace engine {
     }
 
     Json::Value FileManager::ReadJsonFile(const std::string& fileName) {
-        // Json::Value root;
-        // std::ifstream file(fileName);
-        // file >> root;
-        // return root;
+        Json::Value root;
+        std::ifstream file(fileName);
+        file >> root;
+        return root;
     }
     void FileManager::WriteJsonFile(const std::string& fileName) {
-        // std::ofstream file(fileName);
+        std::ofstream file(fileName);
         // file << root;
     }
 
     std::shared_ptr<Component<ModelData_Dynamic, TextureData_Color>> FileManager::ReadComponent(const std::string& fileName) {
-        // Json::Value root = ReadJsonFile(fileName);
+        Json::Value root = ReadJsonFile(fileName);
         printf("[DEBUG] read component\n");
-        // std::shared_ptr<Component<ModelData_Dynamic, TextureData_Color>> component = std::make_shared<Component<ModelData_Dynamic, TextureData_Color>>();
+        std::shared_ptr<Component<ModelData_Dynamic, TextureData_Color>> component = std::make_shared<Component<ModelData_Dynamic, TextureData_Color>>();
+
+        // 여기서 해체하면 됨.
         // component->Deserialize(root);
         return nullptr;
     }
